d937b1e72a0c5e9d340d96ccebaba706ea54af50
rancharaz <ranveer1990@hotmail.com>
2023-04-10 23:18:24 +0400

    logout added

--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -58,10 +58,10 @@ const Signup = () => {
                 <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter name" />
                 {/*  */}
                 <label className="block mt-2 text-2xl">Email: </label>
-                <input type="text" name="name" onChange={(e) => setEmail(e.target.value)} value={email} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter name" />
+                <input type="text" name="name" onChange={(e) => setEmail(e.target.value)} value={email} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter email" />
                 {/*  */}
                 <label className="block mt-2 text-2xl">Password: </label>
-                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter name" />
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter password" />
                 <button type="button" onClick={handleInput} className="bg-indigo-500 p-2 rounded-md text-white mt-4 hover:bg-black focus:bg-black">
                     Submit
                 </button>
--- frontend/src/segments/Nav.jsx
+++ frontend/src/segments/Nav.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 const Nav = () => {
-    const auth = localStorage.getItem('user')
 
+    const auth = localStorage.getItem('user'); /* user's data */
+    const navigate = useNavigate();
+
+
+    /* clear localstorage data to logout */
+    const logout = () => {
+        localStorage.clear();
+        /* navigate to sign-up */
+        navigate('/sign-up')
+    }
     return (
         <nav className='w-full bg-black h-14 flex'>
             <div className="mx-auto container text-center justify-center items-center flex">
@@ -16,8 +25,9 @@ const Nav = () => {
                     <Link to="/update-products" className='pr-4 hover:text-gray-500 transition-all' >Update Product</Link>
 
                     <Link to="/profile" className='pr-4 hover:text-gray-500 transition-all' >Profile</Link>
+                    {/* if auth true hide signup */}
                     {
-                        auth ? <Link to="/logout" className='pr-4 hover:text-gray-500 transition-all' >Logout</Link> : <Link to="/sign-up" className='pr-4 hover:text-gray-500 transition-all' >Sign up</Link>
+                        auth ? <Link onClick={logout} to="/sign-up" className='pr-4 hover:text-gray-500 transition-all' >Logout</Link> : <Link to="/sign-up" className='pr-4 hover:text-gray-500 transition-all' >Sign up</Link>
                     }
 
 
